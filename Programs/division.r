5ea09fca44d65aeca62a4c85172062b9055bb329
“ILARIO-KAGOO” <ilariokagoo5047.sse@saveetha.com>
2023-08-01 14:16:12 +0530

    QN-4

---
+++ Programs/division.r
@@ -0,0 +1,4 @@
+num1 = as.integer(readline(prompt = "Enter the num1:"))
+num2 = as.integer(readline(prompt = "Enter the num2:"))
+print(paste("Division(Quotient) =",num1/num2))
+print(paste("Division(Remainder) =",num1%%num2))
