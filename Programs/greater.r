075fd277a850f154b4e6b29538fd9ca5973232aa
“ILARIO-KAGOO” <ilariokagoo5047.sse@saveetha.com>
2023-08-03 12:18:00 +0530

    QN-8

--- Programs/greater.r
+++ Programs/greater.r
@@ -1,8 +1,13 @@
 num = as.integer(readline(prompt = "Enter the number: "))
-if(num > 3)
+num1 = as.integer(readline(prompt = "Enter the number: "))
+num2 = as.integer(readline(prompt = "Enter the number: "))
+if(num > num1 && num > num2)
 {
-  print(paste(num, "is greater than 3"))
+  print(paste(num, "is greatest of 3"))
+}else if (num1 > num && num1 > num2)
+{
+  print(paste(num1, "is greatest of 3"))
 }else 
 {
-  print(paste(num, "is not greater than 3"))
+  print(paste(num2,"is greatest of 3"))
 }
